09fa2338ada0573828e4ecbaf77c04f162485c19
Francisco Lavin <fcolavin@gmail.com>
2024-01-21 18:44:59 -0300

    Add JQ Explorer script and installation instructions

---
+++ README.md
@@ -0,0 +1,69 @@
+# JQ Explorer
+
+JQ Explorer is a Python script that allows you to interactively explore JSON data using the [jq](https://stedolan.github.io/jq/) command-line tool. It provides a command-line interface where you can enter jq commands and see the results highlighted using the Pygments library.
+
+
+## Installation
+
+To install and run this script, follow these steps:
+
+1. Clone the repository from GitHub:
+
+```
+git clone https://github.com/flavin/jq_explorer.git
+```
+
+2. Navigate to the project directory:
+
+```
+cd jq_explorer
+```
+
+3. Install the required Python packages:
+
+```
+pip install -r requirements.txt
+```
+
+Please ensure that you have Python and pip installed on your system before following these steps.
+
+## Usage
+
+To run the script, use the following command:
+
+```
+echo '{"name": "John", "age": 30, "city": "New York"}' | python jq_explorer/main.py
+```
+
+if you have a file `example.json``
+```
+{
+    "employees": [
+        {
+            "firstName": "John",
+            "lastName": "Doe"
+        },
+        {
+            "firstName": "Anna",
+            "lastName": "Smith"
+        },
+        {
+            "firstName": "Peter",
+            "lastName": "Jones"
+        }
+    ]
+}
+```
+
+then you can
+
+```
+cat example.json | python jq_explorer/main.py
+```
+
+or
+
+```
+curl -s https://pokeapi.co/api/v2/pokemon/  | python3 jq_explorer/main.py
+```
+![Screenshot using previous command](images/curl-example.png)
---
+++ jq_explorer/main.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+import os
+import readline
+import subprocess
+import sys
+import tempfile
+import json
+
+from pygments import highlight, lexers, formatters
+
+EMPTY_MESSAGE = "(EMPTY)"
+INVALID_INPUT_MESSAGE = "Invalid input"
+NO_INPUT_MESSAGE = "No input"
+QUIT_COMMAND = "q"
+
+cache = {}
+
+
+def process_response(response, jq_string):
+    processed_output = None
+    with tempfile.NamedTemporaryFile("w", delete=False) as f:
+        f.write(response)
+    try:
+        processed_output = subprocess.check_output(
+            f"jq -r '{jq_string}' {f.name}", shell=True
+        )
+    except subprocess.CalledProcessError as error:
+        print(f"Command '{error.cmd}' returned non-zero exit status {error.returncode}.")
+        print(f"Command output: {error.output}")
+    finally:
+        os.unlink(f.name)  # ensure the temporary file is deleted
+    return processed_output.decode() if processed_output else None
+
+
+def handle_input(label, prev_value=""):
+    readline.set_startup_hook(
+        lambda: readline.insert_text(prev_value if prev_value else "")
+    )
+    try:
+        result = input(label)
+    finally:
+        readline.set_startup_hook()
+    return result
+
+
+def highlight_output(output, lexer):
+    return highlight(output, lexer, formatters.TerminalFormatter())
+
+
+def get_jq_command():
+    if cache.get("last_jq") is not None:
+        print("Latest jq: ", cache.get("last_jq", "None"))
+    jq_string = handle_input(
+        "Enter jq string to handle the output: ", cache.get("last_jq")
+    )
+    cache["last_jq"] = jq_string
+    return jq_string
+
+
+def handle_response(response):
+    while True:
+        jq_string = get_jq_command()
+        if jq_string == QUIT_COMMAND:
+            break
+
+        processed_output = process_response(response, jq_string)
+        if processed_output is None:
+            print(EMPTY_MESSAGE)
+            continue
+
+        try:
+            json.loads(processed_output)
+            lexer = lexers.JsonLexer()
+        except (json.decoder.JSONDecodeError, TypeError):
+            lexer = lexers.HtmlLexer()
+
+        print(highlight_output(processed_output, lexer))
+
+
+def make_available_the_user_input():
+    sys.stdin.close()
+    sys.stdin = os.fdopen(1)
+
+
+def main():
+    response = sys.stdin.read().strip()  # read the data from the pipe
+    try:
+        json.loads(response)
+    except json.decoder.JSONDecodeError as decode_error:
+        print(f"{INVALID_INPUT_MESSAGE}: {decode_error}")
+        return
+
+    if not response:
+        print(NO_INPUT_MESSAGE)
+        return
+
+    make_available_the_user_input()
+    handle_response(response)
+
+
+if __name__ == "__main__":
+    main()
